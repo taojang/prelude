d6bd91bad0cb99a5c02a799083982cd959bd61f7
Tao Yang <tao.yang@zalando.de>
2016-03-28 17:36:11 +0200

    add haskell configuration

---
+++ personal/init-haskell.el
@@ -0,0 +1,4 @@
+(let ((my-cabal-path (expand-file-name "~/.local/bin")))
+  (setenv "PATH" (concat my-cabal-path path-separator (getenv "PATH")))
+  (add-to-list 'exec-path my-cabal-path))
+(custom-set-variables '(haskell-tags-on-save t))
