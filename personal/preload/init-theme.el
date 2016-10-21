e4ac77585ac36b945c37c0783d28dcdb692153f2
Tao Yang <our1944@gmail.com>
2016-10-21 21:55:01 +0200

    changes font to Fira Mono

--- personal/preload/init-theme.el
+++ personal/preload/init-theme.el
@@ -8,15 +8,15 @@
 ;;                             ))
 
 
-(set-default-font "Fira Code-10")
+;; (set-default-font "Fira Code-10")
 ;; (set-default-font "WenQuanYi Zen Hei Mono-12")
 
-;; (setq initial-frame-alist '(
-;;                             (font . "Fira Code-11")
-;;                             ))
-;; (setq default-frame-alist '(
-;;                             (font . "Fira Code-11")
-;;                             ))
+(setq initial-frame-alist '(
+                            (font . "Fira Mono-11")
+                            ))
+(setq default-frame-alist '(
+                            (font . "Fira Mono-11")
+                            ))
 
 ;; (setq initial-frame-alist '(
 ;;                             (font . "Anonymous Pro for Powerline-13")
