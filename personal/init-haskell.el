0d6e58236b9786ff95a31f31721f5d3e50c5cc75
Tao Yang <tao.yang@zalando.de>
2018-01-03 22:28:35 +0100

    haskell mode setting

--- personal/init-haskell.el
+++ personal/init-haskell.el
@@ -1,23 +1,31 @@
-;(prelude-require-packages '(use-package haskell-mode dante))
-(prelude-require-packages '(haskell-mode))
+(prelude-require-packages '(use-package haskell-mode dante))
+;(prelude-require-packages '(haskell-mode))
 
 (let ((my-cabal-path (expand-file-name "~/.local/bin")))
-  (setenv "PATH" (concat my-cabal-path path-separator (getenv "PATH")))
+      (setenv "PATH" (concat my-cabal-path
+                             path-separator
+                             (getenv "PATH")))
   (add-to-list 'exec-path my-cabal-path)
   ;; nix
   (add-to-list 'exec-path "/run/current-system/sw/bin"))
 
-;; (use-package dante
-;;              :ensure t
-;;              :after haskell-mode
-;;              :commands 'dante-mode
-;;              :init
-;;              (add-hook 'haskell-mode-hook 'dante-mode)
-;;              (add-hook 'haskell-mode-hook 'flycheck-mode))
+(use-package dante
+             :ensure t
+             :after haskell-mode
+             :commands 'dante-mode
+             :bind (("C-c h" . haskell-hoogle))
+             :init
+             (add-hook 'haskell-mode-hook 'dante-mode)
+             (add-hook 'haskell-mode-hook 'flycheck-mode)
+             (add-hook 'haskell-mode-hook 'company-mode))
 
 ;; stack , move to dir-locals latter
-(custom-set-variables '(haskell-process-type 'stack-ghci))
-(custom-set-variables '(haskell-process-args-stack-ghci '("--ghc-options=-ferror-spans")))
-(custom-set-variables '(haskell-process-args-ghci '("--nix" "--system-ghc")))
+(custom-set-variables '(haskell-process-type 'cabal-repl))
+(custom-set-variables '(haskell-process-args-stack-ghci '("--nix" "--system-ghc" "--ghc-options=-ferror-spans")))
+
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
 (setq-default flycheck-disabled-checkers '(haskell-stack-ghc))
+
+(define-key interactive-haskell-mode-map (kbd "M-.") 'haskell-mode-goto-loc)
+(define-key interactive-haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
